cfe173ef541b862bad6bb051ece238ace6ea3078
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-12-16 00:09:59 -0900

    main

---
+++ portfolio/src/component/landingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>hello! this is riyike</h1>
+        <p> </p>
+      </main>
+    </div>
+  );
+}
