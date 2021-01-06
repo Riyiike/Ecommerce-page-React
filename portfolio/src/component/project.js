b9e1d23c26eaa1e49da52162291a855948417d6b
Riyiike <odusanyaiyanuoluwa@gmail.com>
2021-01-05 23:48:35 -0900

    shortner

--- portfolio/src/component/project.js
+++ portfolio/src/component/project.js
@@ -2,7 +2,14 @@ import React from "react ";
 export default function MyPorject() {
   return (
     <div>
-      <div className=""></div>
+      <div className="">
+        {" "}
+        <h1>URL SHORTNER</h1>
+        <p>
+          FUlly functional url shortning website with subpages using HTML, CSS
+          and JS{" "}
+        </p>
+      </div>
     </div>
   );
 }
